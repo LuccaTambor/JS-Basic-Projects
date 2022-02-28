9386270c3aa12667f71f75367401bdd74d5dd872
LuccaTambor <lucca.tambor@outlook.com>
2022-02-28 12:46:33 -0300

    feat: getting url
    extension now gets the url of the page you're in

--- ExtensionJS/index.js
+++ ExtensionJS/index.js
@@ -1,35 +1,44 @@
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const inputEl = document.getElementById("input-el");
 const listEl = document.getElementById("list");
 
 let myLeads = [];
+
 const localLeads = JSON.parse(localStorage.getItem("myLeads"));
 
 if(localLeads) {
     myLeads = localLeads;
-    renderLeads();
+    render(myLeads);
 }
 
 inputBtn.addEventListener("click", saveLead);
-deleteBtn.addEventListener("dblclick", deleteLeads);
+deleteBtn.addEventListener("click", deleteLeads);
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+});
 
 function saveLead() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    renderLeads();
+    render(myLeads);
 }
 
 function deleteLeads () {
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 }
 
-function renderLeads() {
+function render(leads) {
     listEl.innerHTML = " ";
-    myLeads.forEach(element => {
+    leads.forEach(element => {
         let li = document.createElement("li");
         let a = document.createElement("a");
         a.href = element;
